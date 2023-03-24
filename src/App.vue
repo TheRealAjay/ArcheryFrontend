78897273840362cff2d2ddbccd8f01a274d4f05f
Kilian Hinterhölzl <kilianhinterhoelzl@gmail.com>
2023-03-24 12:02:38 +0100

    Added Axios and Bootstrap
    API testing with Axios

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,53 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+// const axios = require('../node_modules/axios/dist/browser/axios.cjs')
+</script>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      testMsg: 'test message',
+      token: '',
+      // url: 'https://localhost:7245//Auth/login',
+      url: 'http://88.198.105.137:5000/Auth/login'
+    }
+  },
+  methods: {
+    test() {
+      this.testMsg = this.testMsg.split('').reverse().join('');
+    },
+    fetchToken() {
+      this.token = axios({
+        url: this.url,
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "*/*",
+          // "Accept-Encoding": "gzip, deflate, br",
+          // "Origin": "http://localhost:5173/"
+        },
+        data: {
+          email: "test@test.test",
+          password: "testtest"
+        }
+      })
+          .then(function(response){
+            return response["token"];
+          })
+          .catch(err => console.log(err))
+
+    }
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div @click="test">{{ token || testMsg }}</div>
+  <br>
+  <button type="button" class="btn btn-primary" @click="fetchToken">Fetch Token</button>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
