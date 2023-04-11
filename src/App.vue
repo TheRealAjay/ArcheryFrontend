e14d160177ebd888bed486bf400d405376ce73c4
Kilian Hinterhölzl <kilianhinterhoelzl@gmail.com>
2023-04-11 20:17:09 +0200

    Added Offcanvas nav sidebar in new component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import Login from './components/Login.vue'
 import Register from "@/components/Register.vue";
 import Dashboard from "@/components/Dashboard.vue";
+import Navigation from "@/components/Navigation.vue";
 </script>
 
 <script>
@@ -22,6 +23,7 @@ export default {
   <div class="container">
     <div class="row">
       <div class="col-12">
+          <Navigation></Navigation>
         <Login @token="(msg) => this.token = msg" v-if="token.length == 0 && !register" @register="(msg) => this.register = msg"></Login>
         <Register v-if="token.length == 0 && register" @registered="(msg)=>register = !msg" @login="()=>register=false"></Register>
         <Dashboard v-if="token.length > 0"></Dashboard>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,8 +1,32 @@
 <template>
+    <nav class="navbar navbar-dark fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
 
+                Archer's Companion</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+<!--todo: Make Username dynamic, add pofile pic (zumindest an placeholder)-->
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Username placeholder</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script>
+import offcanvas from "bootstrap/js/src/offcanvas";
 export default {
   name: "Navigation"
 }
