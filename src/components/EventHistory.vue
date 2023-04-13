e5d906cef4bcb0a0b5e4f7398ae24cc0a36483bf
Ajay <merktmanleicht@gmail.com>
2023-04-13 15:12:06 +0200

    dashboard entwicklung

---
+++ src/components/EventHistory.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row ">
+            <div class="col-12">
+                <div class="header">Eventverlauf</div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="archery-button small" @click="$emit('setShowWindow', 1)">
+                    <div class="outline"></div>
+                    <div class="text">(ICON) Zurück</div>
+                    <div class="overlay red"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "EventHistory",
+    emits: ["setShowWindow"]
+}
+</script>
+
+<style scoped>
+
+</style>
